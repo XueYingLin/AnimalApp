41d1ba75f90d099fd98c540d5ec06c70bfc599dd
Xue Ying Lin <xueying.lin@sjsu.edu>
2021-12-25 19:03:35 -0800

    Fix static serving

--- server/index.js
+++ server/index.js
@@ -49,6 +49,11 @@ app.get('/addAnimal', async function (req, res, next) {
   }
 })
 
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
+})
+
 app.listen(PORT, () => {
   console.log(`server listening on ${PORT}`)
 })
